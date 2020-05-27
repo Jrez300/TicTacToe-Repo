c67138f35c69710f36cf19157034c43d40d363b0
Jrez300 <Joseperez940@live.com>
2020-05-27 11:26:49 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Board from '/Users/Jrez/Desktop/tic-tac-toe/src/components/board/board.js'
+function App() {
+  return (
+    <>
+      <Board />
+    </>
+  );
+}
+export default App;
